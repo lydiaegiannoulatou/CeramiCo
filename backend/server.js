972f7844ceb788d13992153277e1a7370087a7c0
lydiaegiannoulatou <lydiaelliegiannoulatou@gmail.com>
2025-06-14 19:35:03 +0300

    cors fix

--- backend/server.js
+++ backend/server.js
@@ -27,10 +27,7 @@ app.post(
 
 
 app.use(express.json());
-app.use(cors({
-  origin: ['http://localhost:5173', 'https://ceramico.onrender.com'], 
-  credentials: true,
-}));
+app.use(cors());
 
 main();
 
